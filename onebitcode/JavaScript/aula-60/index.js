0f6b23e60b9f499df87e83270d3c19dbf970720c
DavidSant021 <146587018+DavidSant021@users.noreply.github.com>
2023-12-21 14:38:01 -0300

    add aula 60

---
+++ onebitcode/JavaScript/aula-60/index.js
@@ -0,0 +1,27 @@
+const input = document.getElementById('input');
+
+document.getElementById('value').addEventListener('click', function() {
+    input.value = 'Olá, Mundo!'
+
+    console.log(input.value);
+    console.log(input.getAttribute('value'));
+})
+
+document.getElementById('type').addEventListener('click', function() {
+    input.setAttribute('type', 'radio');
+})
+
+document.getElementById('placeholder').addEventListener('click', function() {
+    input.placeholder = 'Digite algo...'
+})
+
+document.getElementById('disable').addEventListener('click', function() {
+    input.setAttribute('disabled', !input.disabled);
+})
+
+document.getElementById('data').addEventListener('click', function() {
+    const data = input.dataset.somethingElse
+    console.log("O valor do atributo data-something-else é: " + data)
+    input.dataset.somethingElse = 'Algum outro valor'
+    console.log("O valor do atributo data-something-else agora é: " + input.dataset.somethingElse)
+})
