1cef27bdfd2345a35bb4e22f85b3b7a4f505be28
DavidSant021 <146587018+DavidSant021@users.noreply.github.com>
2024-01-02 14:28:14 -0300

    terminado módulo 4

--- onebitcode/JavaScript/exercicio-17/webpack.config.js
+++ onebitcode/JavaScript/exercicio-17/webpack.config.js
@@ -1,18 +1,27 @@
+const path = require('path')
+
 module.exports = {
-    entry: {
-      index: './src/index.js'
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
     },
-    mode: 'production',
-    module: {
-      rules: [{
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }, {
-        test: /.js$/,
-        use: ['babel-loader']
-      }]
-    },
-    output: {
-      filename: '[name].min.js'
-    }
+    compress: true,
+    port: 8000
+  },
+  entry: {
+    index: './src/index.js'
+  },
+  mode: 'production',
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }, {
+      test: /.js$/,
+      use: ['babel-loader']
+    }]
+  },
+  output: {
+    filename: '[name].min.js'
+  }
 }
