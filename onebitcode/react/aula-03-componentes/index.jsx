b75da3f98893ac58040b44fdc9c847927f198a69
DavidSant021 <146587018+DavidSant021@users.noreply.github.com>
2024-03-05 21:35:54 -0300

    aula 2 e 3

---
+++ onebitcode/react/aula-03-componentes/index.jsx
@@ -0,0 +1,12 @@
+function PrimaryButton() {
+    return <button>Botão</button>
+}
+
+function Container() {
+    return (
+        <div className="container">
+            <h1>Título</h1>
+            <p>Parágrafo</p>
+        </div>
+    )
+}
