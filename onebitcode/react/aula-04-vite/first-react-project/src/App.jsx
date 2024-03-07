198f6f79948cb13823bc28d0dcab944caa75c447
DavidSant021 <146587018+DavidSant021@users.noreply.github.com>
2024-03-07 17:28:59 -0300

    exercicio 1

---
+++ onebitcode/react/aula-04-vite/first-react-project/src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Este é meu primeiro projeto usando React e Vite</p>
+      <p>Próximos passos:</p>
+      <ul>
+        <li>Praticar com exercícios</li>
+        <li>Aprender a usar dados dinâmicos</li>
+        <li>Criar novos componentes</li>
+        <li>Estilizar os componentes</li>
+        <li>Reutilizar os componentes</li>
+      </ul>
+    </div>
+  )
+}
---
+++ onebitcode/react/exercicio-01/exx01/src/App.jsx
@@ -0,0 +1,29 @@
+export default function App() {
+    return (
+        <div className="app">
+            <img src="/react.png" alt="Logo React" />
+            <h1>React</h1>
+            <p>A biblioteca para interfaces de usuário web e nativas.</p>
+            <div>
+                <button>Aprenda React</button>
+                <button>Referência da API</button>
+            </div>
+            <hr />
+            <h2>Crie interfaces de usuário de componentes</h2>
+            <p>React permite que você construa interfaces de usuário a patir de pedaços individuais chamados componentes.</p>
+            <hr />
+            <h2>Escreva os componentes com código e marcação</h2>
+            <p>Componentes React são funções JavaScript. A sintaxe de marcação é chamada de JSX. É uma estensão da sintaxe do JavaScript popularizada pelo React.</p>
+            <hr />
+            <h2>Próximos passos</h2>
+            <ul>
+                <li>Uso de dados dinâmicos no JSX</li>
+                <li>Criação de novos componentes</li>
+                <li>Estilização de componentes</li>
+                <li>Reutilização de componentes</li>
+                <li>Uso de props e children</li>
+                <li>Uso de eventos do JavaScript</li>
+            </ul>
+        </div>
+    )
+}
