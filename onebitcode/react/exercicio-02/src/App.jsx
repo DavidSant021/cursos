1b701bd7bb8e3bd0375f7ca8cad5231f8bab7558
DavidSant021 <146587018+DavidSant021@users.noreply.github.com>
2024-03-09 17:22:56 -0300

    att do exercicio 2

--- onebitcode/react/exercicio-02/src/App.jsx
+++ onebitcode/react/exercicio-02/src/App.jsx
@@ -2,13 +2,7 @@ import Card from "./components/Card"
 
 export default function App() {
   return (
-    <div style={{
-      width: "100vw",
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center"
-    }}>
+    <div className="app">
       <Card />
     </div>
   )
--- onebitcode/react/exercicio-02/src/components/Card.jsx
+++ onebitcode/react/exercicio-02/src/components/Card.jsx
@@ -1,15 +1,14 @@
 import styles from './Card.module.css'
+import posterImg from '../../public/starwars.jpg'
 
 export default function Card() {
     return (
         <div className={styles.container}>
-            <div className={styles.imgContent}>
-                <img src=".././starwars.jpg" alt="Pôster do Star Wars" />
-            </div>
-            <div className={styles.textContent}>
-                <h1>Pôster: Star Wars (1977)</h1>
-                <p>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
-                <button>Comprar Agora</button>
+            <img className={styles.poster} src={posterImg} alt="Pôster do Star Wars" />
+            <div>
+                <h2 className={styles.title}>Pôster: Star Wars (1977)</h2>
+                <p className={styles.description}>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
+                <button className={styles.button}>Comprar Agora</button>
             </div>
         </div>
     )
