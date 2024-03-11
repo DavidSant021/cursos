23cf4ee6eab283b25cb4cd3429e0e228a9c07ff0
DavidSant021 <146587018+DavidSant021@users.noreply.github.com>
2024-03-11 13:06:37 -0300

    modificado ex 03

--- onebitcode/react/exercicio-03/src/App.jsx
+++ onebitcode/react/exercicio-03/src/App.jsx
@@ -4,8 +4,16 @@ import Profile from "./components/Profile"
 
 export default function App() {
   return (
-    <>
-      <Profile avatar={avatar} name="David Santiago" bio="Full-stack javascript developer at Acme Inc." phone="+55 11987654321" email="devdavidsant21@gmail.com" githubUrl="https://github.com/DavidSant021" linkedinUrl="https://www.linkedin.com/in/david-santiago-251728292/" twitterUrl="https://twitter.com/?lang=pt-br" />
-    </>
+    <div className="app">
+      <Profile 
+        avatar={avatar}
+        name="David Santiago" 
+        bio="Full-stack javascript developer at Acme Inc."
+        phone="+55 11987654321"
+        email="devdavidsant21@gmail.com"
+        githubUrl="https://github.com/DavidSant021"
+        linkedinUrl="https://www.linkedin.com/in/david-santiago-251728292/" twitterUrl="https://twitter.com/?lang=pt-br"
+       />
+    </div>
   )
 }
--- onebitcode/react/exercicio-03/src/components/Profile/index.jsx
+++ onebitcode/react/exercicio-03/src/components/Profile/index.jsx
@@ -1,24 +1,18 @@
-import style from "./style.module.css"
+import styles from "./style.module.css"
 
 export default function Profile({avatar, name, bio, phone, email, githubUrl, linkedinUrl, twitterUrl}) {
     return (
-        <div className={style.container}>
-            <img className={style.avatarImg} src={avatar} alt="Foto" />
-            <div className={style.div}>
-                <h1 className={style.titleContent}>{name}</h1>
+        <div className={styles.container}>
+            <img className={styles.avatar} src={avatar} alt={name} />
+            <h2 className={styles.name}>{name}</h2>
+            <div>{bio}</div>
+            <div>{phone}</div>
+            <div>{email}</div>
+            <div className={styles.links}>
+                <a href={githubUrl} target="_blank">GitHub</a>
+                <a href={linkedinUrl} target="_blank">LinkedIn</a>
+                <a href={twitterUrl} target="_blank">Twitter</a>
             </div>
-            <div className={style.div}>
-                <p className={style.informations}>{bio}</p>
-            </div>
-            <div className={style.div}>
-            <p className={style.informations}>{phone}</p>
-            </div>
-            <div className={style.div}>
-            <p className={style.informations}>{email}</p>
-            </div>
-            <a style={{marginTop: "10px"}} className={style.socialMedia} href={githubUrl} target="_blank" >GitHub</a>
-            <a className={style.socialMedia} href={linkedinUrl} target="_blank" >LinkedIn</a>
-            <a className={style.socialMedia} href={twitterUrl} target="_blank" >Twitter</a>
         </div>
     )
 }
