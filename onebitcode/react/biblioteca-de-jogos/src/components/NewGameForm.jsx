be8f3846780aa2158b5f724ebd4a992334374028
DavidSant021 <146587018+DavidSant021@users.noreply.github.com>
2024-03-22 21:16:11 -0300

    terminado projeto

---
+++ onebitcode/react/biblioteca-de-jogos/src/components/NewGameForm.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import PropTypes from 'prop-types'
+import TextInput from "./TextInput";
+
+NewGameForm.propTypes = {
+    addGame: PropTypes.func
+}
+
+export default function NewGameForm({addGame}) {
+
+    const [title, setTitle] = useState('');
+    const [cover, setCover] = useState('');
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault();
+        addGame({title, cover})
+        setTitle('');
+        setCover('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+        <TextInput id="title" label="Título:" value={title} setValue={setTitle}/>
+        <TextInput id="cover" label="Capa:" value={cover} setValue={setCover}/>
+        <button type="submit">Adicionar à biblioteca</button>
+      </form>
+    )
+}
---
+++ onebitcode/react/biblioteca-de-jogos/src/components/TextInput.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types"
+
+TextInput.propTypes = {
+    id: PropTypes.string,
+    label: PropTypes.string,
+    value: PropTypes.string,
+    setValue: PropTypes.func
+}
+
+export default function TextInput({id, label, value, setValue }) {
+    return (
+        <div>
+          <label htmlFor={id}>{label}</label>
+          <input 
+            type="text"
+            name={id} 
+            id={id}
+            value={value}
+            onChange={(e) => setValue(e.target.value)} />
+        </div>
+    )
+}
