cb291f90f4a87516c19272ce02afc7b82e627578
DavidSant021 <146587018+DavidSant021@users.noreply.github.com>
2024-04-09 23:25:01 -0300

    renderização do lado do servidor

--- onebitcode/next/black-nextjs/src/pages/index.tsx
+++ onebitcode/next/black-nextjs/src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${inter.className}`}>
         <h1>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
