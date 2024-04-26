f6673b4e2f074c2598a07d6de62ab11f09c6854a
DavidSant021 <146587018+DavidSant021@users.noreply.github.com>
2024-04-26 20:43:38 -0300

    aula 7

---
+++ onebitcode/node/aula-07-argumentos-de-linha-de-comando/index.js
@@ -0,0 +1,13 @@
+const args = process.argv
+
+const namedArguments = {}
+
+process.argv.slice(2).forEach((arg, index, array) => {
+    if (arg.startsWith("--")) {
+        const argName = arg.slice(2)
+        const argValue = array[index + 1]
+        namedArguments[argName] = argValue
+    }
+})
+
+console.log("Argumentos informados: ", namedArguments)
