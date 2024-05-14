5f7d4c0b3f9020d5265267772662520181365fe3
DavidSant021 <146587018+DavidSant021@users.noreply.github.com>
2024-05-13 21:35:22 -0300

    redirecionamento

--- onebitcode/node/aula-12-html-dinamico-com-ejs/src/index.js
+++ onebitcode/node/aula-12-html-dinamico-com-ejs/src/index.js
@@ -3,14 +3,36 @@ const path = require('node:path')
 
 const app = express()
 
+const storedUsers = []
+
+// Configuração do EJS
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
+//Configuração do body
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     const title = 'Homepage'
     const message = 'Mensagem dinâmica inserida pelo EJS'
 
     res.render('index', {title, message})
+})
+
+app.get('/formulario', (req, res) => {
+    res.render('form')
+})
+
+app.post('/register', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    storedUsers.push({username, password})
+    res.redirect('/usuarios')
+})
+
+app.get('/usuarios', (req, res) => {
+    res.render('users', {users: storedUsers})
 })
 
 const PORT = 3000
