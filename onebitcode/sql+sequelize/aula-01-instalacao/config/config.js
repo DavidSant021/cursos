0fdc50b297573cf5b254c56445fbac4922f11014
DavidSant021 <146587018+DavidSant021@users.noreply.github.com>
2024-07-17 22:57:36 -0300

    sql

---
+++ onebitcode/sql+sequelize/aula-01-instalacao/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    port: 2002,
+    username: "postgres",
+    password: "26324915",
+    database: "curso_sequelize",
+    define: {
+      timestamps: true,
+    },
+  };
